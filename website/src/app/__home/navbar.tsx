8223a8f6ea5636c89f1c1bd0d8ca23575b0345b6
Thirunavukkarasu Muthusamy <thiru@mantys.io>
2024-09-30 16:58:53 +0530

    fixed branding name

--- website/src/app/__home/navbar.tsx
+++ website/src/app/__home/navbar.tsx
@@ -6,9 +6,9 @@ export default async function Navbar() {
     <nav className="p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <a href="#" className="text-2xl font-bold">
-          Garment
+          War
           <span className="bg-gradient-to-r from-blue-500 to-blue-600 bg-clip-text text-transparent mt-1 inline-flex">
-            Khazana
+            dro
           </span>
         </a>
         <div className="hidden md:flex space-x-4">
